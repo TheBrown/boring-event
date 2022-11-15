07961860046370fd61eae7d5baeb49be841f3ce2
saleumsack keoboualay <saleumsack.bcel@gmail.com>
2022-11-15 08:04:16 +0700

    Add tailwind

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Find your best reason ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement  = screen.getByText(/Generate/i);
+  expect(buttonElement).toBeInTheDocument();
 });
